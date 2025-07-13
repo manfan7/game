d487db17095a3176538e015587c745fc21973404
igor grytsuk <igorgrytsuk@yandex.ru>
2025-07-13 12:21:01 +0300

    lats edits

---
+++ constants.js
@@ -0,0 +1,11 @@
+export const GameStatus = {
+    pending: 'pending',
+    inProgress: 'in_progress',
+    Win:'Win'
+}
+export const directions ={
+    UP:'UP',
+    LEFT:'LEFT',
+    RIGHT:'RIGHT',
+    DOWN:'DOWN'
+}
