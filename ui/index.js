5602f37bdbe24f72861274c29ada2300c5225bf0
Hrytsuki <igorgrytsuk@yandex.ru>
2025-07-29 01:12:59 +0300

    last changes back

--- ui/index.js
+++ ui/index.js
@@ -1,15 +1,11 @@
 import {Controller} from "./controller.js";
 import {View} from "./view.js";
-import {Game, PositionService, GridSize, Player} from "../core/game.js";
-import {Randomizer} from '../core/utils.js'
+import {GameProxy} from "./game-proxy.js";
 
 
 const view = new View();
-const numberUtility = new Randomizer();
 
-const settings = new GridSize()
-const positionService = new PositionService(settings, numberUtility)
-const game = new Game(numberUtility, settings, positionService);
+const game = new GameProxy();
 
 const controller = new Controller(view, game);
 controller.init()
